214fac05d5fa118e8d0727684fe5011948b00de8
vvr273 <vvr2732004@gmail.com>
2026-05-12 14:06:39 +0530

    chore(mail): add smtp verify and masked config diagnostics

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import app from "./app.js";
 import { validateEnv } from "./config/validateEnv.js";
+import { verifySmtpConnection } from "./utils/sendEmail.js";
 
 dotenv.config();
 validateEnv();
+verifySmtpConnection();
 const PORT = process.env.PORT || 5000;
 
 
